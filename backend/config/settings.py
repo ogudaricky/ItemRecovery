9c78354e01cce1ae3a1161d8d4ad6ce0d8c6f4c5
Renn <youngrenn001@gmail.com>
2026-04-12 17:13:46 +0300

    Add default authentication classes for Django REST framework

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,3 +139,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+}
+
