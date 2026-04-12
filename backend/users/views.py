008f5dfb70e08385d014b001369face4ec7d43b5
Renn <youngrenn001@gmail.com>
2026-04-12 17:16:42 +0300

    Implement user permissions and add 'me' action to UserViewSet

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import UserSerializer
 
 User = get_user_model()
@@ -7,4 +9,15 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [permissions.AllowAny()]
+        if self.action == "me":
+            return [permissions.IsAuthenticated()]
+        return [permissions.IsAuthenticated()]
+
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
