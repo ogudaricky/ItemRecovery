66092dc7e6530ab5622ad71e660345b010d9c3ea
Renn <youngrenn001@gmail.com>
2026-04-13 15:27:49 +0300

    Add CORS configuration to allow credentials and specify trusted origins for CSRF protection in settings.py.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,6 +67,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 ROOT_URLCONF = 'config.urls'
 
