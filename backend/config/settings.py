76ec5fa85a3b65de5838f1ae40f16bc4c762eeb5
Renn <youngrenn001@gmail.com>
2026-04-13 18:20:05 +0300

    Replace SessionAuthentication with CsrfExemptSessionAuthentication in REST framework settings for improved CSRF handling.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "users.authentication.CsrfExemptSessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
 }
